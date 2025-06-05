762948f1793478b6281aaf362829d8bf18ee1b8f
NguyenAnThienBao <Alexnguyen3217@gmail.com>
2025-06-06 04:00:29 +0700

    created customer model

---
+++ src/modals/customer.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema(
+    {
+        name: {
+            require: true,
+            type: String,
+        },
+        address: String,
+        phone: String,
+        email: String,
+        image: String,
+        description: String,
+    },
+    { timestamps: true },
+);
+
+const Customer = new mongoose.model('Customer', customerSchema);
+
+module.exports = Customer;
